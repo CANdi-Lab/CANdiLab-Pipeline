fff5894568570ecbd00f8901e1bf61bb07b4fb9c
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2025-06-27 21:29:39 -0400

    script to create the template folder

---
+++ scripts/folder_creation.R
@@ -0,0 +1,15 @@
+install.packages("usethis")
+library(usethis)
+
+#Create the template folder structure
+dir.create("data")
+dir.create("data/raw")
+dir.create("data/processed")
+dir.create("data/final")
+dir.create("scripts")
+dir.create("scripts/scoring")
+dir.create("scripts/analysis")
+dir.create("docs")
+dir.create("output")
+dir.create("output/figures")
+
